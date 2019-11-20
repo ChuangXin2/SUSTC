96f7ed7625fa9ba334bb790f0e49aa94cfe18987
lyx <747445407@qq.com>
2019-11-20 11:08:38 +0800

    new input-card download and choose

--- js/csShow.js
+++ js/csShow.js
@@ -6,11 +6,30 @@ map.on('click', function(e) {
 	openInfo();
 });
 function openInfo(){
+	closeInfo2();
+	closeInfo3();
 	document.getElementById('info').style.display='block';
 }
 function closeInfo() {
 	document.getElementById('info').style.display='none';
 }
-
+function openInfo2(){
+	closeInfo();
+	closeInfo3();
+	document.getElementById('download').style.display='block';
+}
+function closeInfo2() {
+	document.getElementById('download').style.display='none';
+}
+function openInfo3(){
+	closeInfo();
+	closeInfo2();
+	document.getElementById('choose').style.display='block';
+}
+function closeInfo3() {
+	document.getElementById('choose').style.display='none';
+}
 document.getElementById("clickOn").onclick = openInfo;
 document.getElementById("clickOff").onclick = closeInfo;
+document.getElementById("clickOn2").onclick = openInfo2;
+document.getElementById("clickOn3").onclick = openInfo3;
